1686de4f1b6d77952ec8d39d01eca24164a318cf
Karel Vaculik <vacullik@gmail.com>
2015-10-27 12:07:48 +0100

    Add configuration class InMemoryDatabaseApplicationContext

---
+++ PersistenceLayer/src/test/java/cz/muni/fi/pa165/WeaponDaoTest.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+/**
+ * Created by vaculik on 26.10.15.
+ */
+
+@ContextConfiguration(classes=InMemoryDatabaseApplicationContext.class)
+public class WeaponDaoTest extends AbstractTestNGSpringContextTests {
+
+}
