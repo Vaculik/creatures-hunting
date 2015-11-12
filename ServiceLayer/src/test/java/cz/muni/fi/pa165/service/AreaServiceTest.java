3cc44c587f12bb1601ad78474bfc02969a484c60
zborilmartin <zboranek@gmail.com>
2015-11-12 14:29:47 +0100

    Structure of ServiceLayer, .gitignore

---
+++ ServiceLayer/src/test/java/cz/muni/fi/pa165/service/AreaServiceTest.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.service;
+
+ /* This class test functionality of methods of Area Data Access Object class.
+ *
+ * @author xxx
+ */
+public class AreaServiceTest{
+
+   
+}
