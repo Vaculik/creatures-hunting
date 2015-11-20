6b0ebbf3ce357b8d475ecdb6c82d626e50ccfcc7
Karel Vaculik <vacullik@gmail.com>
2015-11-20 20:52:06 +0100

    Add CreatureType enum to ApiLayer

--- ApiLayer/src/main/java/cz/muni/fi/pa165/dto/CreatureDTO.java
+++ ApiLayer/src/main/java/cz/muni/fi/pa165/dto/CreatureDTO.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.dto;
 
+
 import cz.muni.fi.pa165.enums.CreatureType;
 
 /**
---
+++ ApiLayer/src/main/java/cz/muni/fi/pa165/enums/CreatureType.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.enums;
+
+/**
+ * @author Karel Vaculik
+ */
+public enum CreatureType {
+    UNDEAD,
+    VAMPIRE,
+    BEAST
+}
