66fc9a602c629f3efc680ade7a2ce8acf5fdd079
Karel Vaculik <vacullik@gmail.com>
2015-11-20 19:35:15 +0100

    Add CreatureFacade interface

--- ApiLayer/src/main/java/cz/muni/fi/pa165/facade/CreatureFacade.java
+++ ApiLayer/src/main/java/cz/muni/fi/pa165/facade/CreatureFacade.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pa165.facade;
 
 import cz.muni.fi.pa165.dto.CreatureDTO;
+import cz.muni.fi.pa165.enums.CreatureType;
 
 import java.util.List;
 
@@ -9,15 +10,71 @@ import java.util.List;
  */
 public interface CreatureFacade {
 
+    /**
+     * Find a CreatureDTO by id.
+     *
+     * @param id id of the CreatureDTO
+     * @return the found CreatureDTO or null if doesn't exist
+     */
     public CreatureDTO getCreatureById(Long id);
 
+    /**
+     * Find a CreatureDTO by name.
+     *
+     * @param name name of the CreatureDTO
+     * @return the found CreatureDTO or null if doesn't exist
+     */
     public CreatureDTO getCreatureByName(String name);
 
+    /**
+     * Create the CreatureDTO.
+     *
+     * @param creatureDTO the CreatureDTO to be created
+     * @return id of the created CreatureDTO
+     */
     public Long createCreature(CreatureDTO creatureDTO);
 
+    /**
+     * Delete the CreatureDTO.
+     *
+     * @param creatureDTO the CreatureDTO to be deleted
+     */
     public void deleteCreature(CreatureDTO creatureDTO);
 
+    /**
+     * Update the CreatureDTO.
+     *
+     * @param creatureDTO the CreatureDTO to be updated
+     * @return id of the updated CreatureDTO
+     */
     public Long updateCreature(CreatureDTO creatureDTO);
 
+    /**
+     * Find all CreatureDTOs.
+     *
+     * @return list of the results
+     */
     public List<CreatureDTO> getAllCreatures();
+
+    /**
+     * Find all CreatureDTOs of a given type.
+     *
+     * @param type the type
+     * @return list of the results
+     */
+    public List<CreatureDTO> getCreaturesOfType(CreatureType type);
+
+    /**
+     * Find all CreatureDTOs with the biggest height.
+     *
+     * @return list of the results
+     */
+    public List<CreatureDTO> getCreaturesWithMaxHeight();
+
+    /**
+     * Find all CreatureDTOs with the biggest weight.
+     *
+     * @return list of the results
+     */
+    public List<CreatureDTO> getCreaturesWithMaxWeight();
 }
