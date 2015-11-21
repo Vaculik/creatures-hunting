6ee75ed660fcfe03530fa8ff2c352586de27351a
Jakub Miculka <miculkaj@gmail.com>
2015-11-21 18:26:41 +0100

    Added UserSystemDTO.java and UserSystemFacade.java

---
+++ ApiLayer/src/main/java/cz/muni/fi/pa165/facade/UserSystemFacade.java
@@ -0,0 +1,80 @@
+package cz.muni.fi.pa165.facade;
+
+import java.util.List;
+
+import cz.muni.fi.pa165.dto.CreatureDTO;
+import cz.muni.fi.pa165.dto.UserSystemDTO;
+import cz.muni.fi.pa165.enums.CreatureType;
+import cz.muni.fi.pa165.enums.SexType;
+import cz.muni.fi.pa165.enums.UserType;
+
+public interface UserSystemFacade {
+	
+    /**
+     * This method creates a user.
+     *
+     * @param user UserSystemDTO to be created
+     */
+    public Long createUser(UserSystemDTO user);
+    
+    /**
+     * This method deletes a user.
+     *
+     * @param area user to be deleted
+     */
+    public void deleteUser(Long userId);
+    
+    /**
+     * This method updates a user.
+     *
+     * @param area UserSystemDTO to be updated
+     */
+    public Long updateUser(UserSystemDTO user);
+    
+    /**
+     * Find all Users.
+     *
+     * @return list of the results
+     */
+    public List<UserSystemDTO> getAllUsers();
+
+    /**
+     * Find all users of given type.
+     *
+     * @param type the type
+     * @return list of the results
+     */
+    public List<UserSystemDTO> getUsersOfType(UserType type);
+    
+    /**
+     * Find all users of given sex.
+     *
+     * @param type the type
+     * @return list of the results
+     */
+    public List<UserSystemDTO> getUsersOfSex(SexType sex);
+    
+    /**
+     * Find a User by id.
+     *
+     * @param id id of the UserSystem
+     * @return the found UserSystemDTO or null if doesn't exist
+     */
+    public UserSystemDTO getUserById(Long id);
+
+    /**
+     * Find a User by name.
+     *
+     * @param name name of the UserSystem
+     * @return the found UserSystemDTO or null if doesn't exist
+     */
+    public UserSystemDTO getUserByName(String name);
+    
+    /**
+     * Find a User by its username.
+     *
+     * @param userName userName of the User
+     * @return the found UserSystemDTO or null if doesn't exist
+     */
+    public UserSystemDTO getUserByUserName(String userName);
+}
