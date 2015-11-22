5ff55b760619441e1384e745125d5f3f52819b93
Karel Vaculik <vacullik@gmail.com>
2015-11-22 18:05:02 +0100

    Add WeaponService interface and WeaponServiceImpl

---
+++ ServiceLayer/src/main/java/cz/muni/fi/pa165/service/WeaponService.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.service;
+
+
+import cz.muni.fi.pa165.entity.Weapon;
+
+public interface WeaponService {
+
+    Weapon getWeaponById(Long id);
+}
---
+++ ServiceLayer/src/main/java/cz/muni/fi/pa165/service/WeaponServiceImpl.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.entity.Weapon;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class WeaponServiceImpl implements WeaponService {
+
+    @Override
+    public Weapon getWeaponById(Long id) {
+        return null;
+    }
+}
