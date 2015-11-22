7601930b227dde250cb751567033d9db7e567dd8
Karel Vaculik <vacullik@gmail.com>
2015-11-22 14:09:03 +0100

    Add WeaponEfficiencyDTO

---
+++ ApiLayer/src/main/java/cz/muni/fi/pa165/dto/WeaponDTO.java
@@ -0,0 +1,6 @@
+package cz.muni.fi.pa165.dto;
+
+
+public class WeaponDTO {
+    // TODO
+}
---
+++ ApiLayer/src/main/java/cz/muni/fi/pa165/dto/WeaponEfficiencyDTO.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165.dto;
+
+/**
+ * Created by vaculik on 22.11.15.
+ */
+public class WeaponEfficiencyDTO {
+
+    private Long id;
+
+    private Integer efficiency;
+
+    private CreatureDTO creature;
+
+    private WeaponDTO weapon;
+
+
+}
