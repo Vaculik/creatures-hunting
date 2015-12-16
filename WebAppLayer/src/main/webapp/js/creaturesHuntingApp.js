23f762c844fa22decdbc16ff44c157b6c07a8ca6
Karel Vaculik <vacullik@gmail.com>
2015-12-16 20:26:39 +0100

    Add creatures of type table to particular creature page

--- WebAppLayer/src/main/webapp/js/creaturesHuntingApp.js
+++ WebAppLayer/src/main/webapp/js/creaturesHuntingApp.js
@@ -51,6 +51,15 @@ controllers.controller('CreaturesController', function($http, $routeParams, $sco
 });
 
 
+var creaturesOfType = function (type, $http, $scope) {
+    console.log('GET creatures of type=' + type);
+    $http.get('/creatures-hunting/rest/creatures/type/' + type).
+        then(function (response) {
+            $scope.typeCreatures = response.data['_embedded']['creatures'];
+        });
+};
+
+
 
 controllers.controller('ParticularCreatureController', function ($http, $rootScope, $location, $routeParams, $scope) {
     var creatureId = $routeParams.creatureId;
@@ -58,15 +67,19 @@ controllers.controller('ParticularCreatureController', function ($http, $rootSco
     $http.get('/creatures-hunting/rest/creatures/' + creatureId).
         then(function(response) {
             $scope.creature = response.data;
+            creaturesOfType(response.data.type, $http, $scope);
+            console.log($scope.typeCreatures);
         },
         function error (response) {
             $rootScope.warningAlert = 'Problem occured when load creature ' + response.data.message;
         });
+
     console.log('GET most effective weapons to creature with id=' + creatureId);
     $http.get('/creatures-hunting/rest/weapon-efficiencies/most-effective-to/creature/' + creatureId).
         then(function(response) {
             $scope.mostEffective = response.data['_embedded']['weapons'];
         });
+
     $scope.delete = function(id) {
         console.log('Delete creature with id=' + id);
         $http.delete('/creatures-hunting/rest/creatures/' + id).
@@ -95,17 +108,14 @@ controllers.controller('NewCreatureController', function($http, $rootScope, $loc
     };
     $scope.create = function(creature) {
         console.log('Create creature: ' + creature.name);
-        $http({
-            method: 'POST',
-            url: '/creatures-hunting/rest/creatures/create',
-            data: creature
-        }).then(function success(response) {
-            $rootScope.succesAllert = 'The new creature was created.';
-            $location.path('/creatures/all');
-        }, function error(response) {
-            console.log('Error when creating new creature');
-            console.log(response);
-            $rootScope.errorAlert = 'Problem has occured, cannot create new creature!';
-        });
+        $http.post('/creatures-hunting/rest/creatures/create',creature).
+            then(function success(response) {
+                $rootScope.succesAllert = 'The new creature was created.';
+                $location.path('/creatures/all');
+            }, function error(response) {
+                console.log('Error when creating new creature');
+                console.log(response);
+                $rootScope.errorAlert = 'Problem has occured, cannot create new creature!';
+            });
     };
 });
