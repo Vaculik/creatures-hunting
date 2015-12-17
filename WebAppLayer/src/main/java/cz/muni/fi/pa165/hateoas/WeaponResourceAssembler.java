7728034c02be039b088c58045ffa351a85d2043d
Pavel-vesely <nuviael@gmail.com>
2015-12-17 19:36:55 +0100

    Weapon REST implemented build/plugin/tomcat7/url added to pom.xml

--- WebAppLayer/src/main/java/cz/muni/fi/pa165/hateoas/WeaponResourceAssembler.java
+++ WebAppLayer/src/main/java/cz/muni/fi/pa165/hateoas/WeaponResourceAssembler.java
@@ -2,11 +2,25 @@ package cz.muni.fi.pa165.hateoas;
 
 import cz.muni.fi.pa165.controllers.WeaponRestController;
 import cz.muni.fi.pa165.dto.WeaponDTO;
+import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 
+/**
+ * 
+ * @author Pavel Vesely <448290@mail.muni.cz>
+ */
 @Component
-public class WeaponResourceAssembler extends ResourceAssemblerSupport<WeaponDTO, WeaponResource>{
+public class WeaponResourceAssembler extends ResourceAssemblerSupport<WeaponDTO, WeaponResource> {
+
+    private static final Logger logger = LoggerFactory.getLogger(WeaponResourceAssembler.class);
+    @Autowired
+    private EntityLinks entityLinks;
 
     public WeaponResourceAssembler() {
         super(WeaponRestController.class, WeaponResource.class);
@@ -14,7 +28,15 @@ public class WeaponResourceAssembler extends ResourceAssemblerSupport<WeaponDTO,
 
     @Override
     public WeaponResource toResource(WeaponDTO weaponDTO) {
-        // TODO
-        return null;
+        WeaponResource weaponResource = new WeaponResource(weaponDTO);
+        Long id = weaponDTO.getId();
+        try {
+            weaponResource.add(entityLinks.linkToSingleResource(WeaponDTO.class, id).withSelfRel());
+            Method delete = WeaponRestController.class.getMethod("deleteWeapon", long.class);
+            weaponResource.add(ControllerLinkBuilder.linkTo(WeaponRestController.class, delete, id).withRel("delete"));
+        } catch (Exception e){
+            logger.error("Failed to create links");
+        }
+        return weaponResource   ;
     }
 }
